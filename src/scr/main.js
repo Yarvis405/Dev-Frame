fff9419c7c65fdb8218b63b68d5f6554a31f614f
Yarvis405 <bucle3512@gmail.com>
2025-04-29 23:13:43 +0000

    features and bugs

--- src/scr/main.js
+++ src/scr/main.js
@@ -15,7 +15,7 @@ const changeTheme = (props = {}) => {
   //console.log(dark.checked)
   document.cookie = JSON.stringify({ theme: dark.checked })
   const { theme } = JSON.parse(document.cookie)
-  //console.log(theme)
+  //console.(theme)
 
   const toDark = document.querySelectorAll(/*props.elements ||*/ ".mainContainer, .fontPrimary, titleHead, .fontSecondary, .projectCard");
 
@@ -87,17 +87,19 @@ const githubAPIWrapper = props => {
   return {
     api: props.api,
     user: props.user,
+    status: 200,
     repos: [],
 
     async init() {
       const thumbnail = `https://raw.githubusercontent.com/${props.user}`;
       const path = `refs/heads/master`;
-      const res = await fetch(`${props.api}/${props.user}/repos`);
+      const altPath = `refs/heads/main`
+      const res = await fetch(`${props.api}/${props.user}/repos`)
       const data = await res.json();
       //console.log(data);
 
       data.forEach(repo => {
-        if (repo.private !== true) {
+        if (repo.private !== true && repo.disabled !== true) {
           this.repos.push({
             id: repo.node_id,
             owner: repo.owner.login,
@@ -105,13 +107,112 @@ const githubAPIWrapper = props => {
             description: repo.description,
             uri: repo.svn_url, //project link;
             host: repo.homepage || "", //if the project is hosted where;
-            thumbnail: `${thumbnail}/${repo.name}/${path || 'refs/heads/main'}/thumbnail.webp` //extract from raw.githubusercountent.com
+            thumbnail: `${thumbnail}/${repo.name}/${path || altPath}/thumbnail.webp` //extract from raw.githubusercountent.com
           })
         }
       })
-    }
+    },
+
   }
 }
+
+
+
+const formHandler = props => {
+  return {
+    form: props.mail,
+    status: {
+      isSent: false,
+      message: '',
+      devMessage: ''
+    },
+    tries: 0,
+
+    compose(host) {
+      //formSubmit api composed mail
+      host = host || 'ebencharles15@gmail.com';
+
+      const toHost = new FormData();
+      toHost.append('_subject', `You Have Recived a New Request #${this.form.service}`)
+      toHost.append('message', /*{image log}*/ `\n${location.origin} ${new Date()} \n\n A ${this.form.service} service have been requested by ${this.form.email}`)
+      //Optional if no mail is given it defaults to no reply.
+      //toHost.append('email', 'no-reply@formsubmit.com')
+
+
+      const toUser = new FormData();
+      toUser.append('_subject', `Your #${this.form.service} request to ${location.origin} have been recived`)
+      toUser.append('message', /*{image log}*/ `\n${location.origin}   ${new Date()} \n\n Your ${this.form.service} request have been recived, and it is being revised, you will be contacted soon. \nIn case of any doubts email me me at ${props.host} \n\natt: Eben C.`)
+      //Optional if no mail is given it defaults to no reply.
+      //toUser.append('email', 'no-reply@formsubmit.com')
+
+      fetch(`https://formsubmit.co/${this.form.email}`, {
+        method: 'POST',
+        body: toUser,
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).then(res => {
+        if (res.ok) {
+          this.status = {
+            isSent: true,
+            message: "Your request have been successfully made."
+          }
+
+        } else {
+          this.status = {
+            isSent: false,
+            message: "Failed to complete your request."
+          }
+
+          //console.log(this.status.message)
+          //localStorage.setItem('status', JSON.stringify(this.status))
+
+          this.tries++
+
+          if (this.tries < 2) {
+            this.tries = 0;
+            return
+          }
+        }
+      });
+
+      if (this.status.isSent) {
+        fetch(`https://formsubmit.co/${host}`, {
+          method: 'POST',
+          body: toHost,
+          headers: {
+            'Accept': 'application/json'
+          }
+        }).then(res => {
+          if (res.ok) {
+            this.status = {
+              isSent: true,
+              devMessage: "Your request have been successfully made."
+            }
+
+          } else {
+            this.status = {
+              isSent: false,
+              devMessage: "Failed to complete your request."
+            }
+
+            //console.log(this.status.devMessage)
+            //localStorage.setItem('status', JSON.stringify(this.status))
+
+            this.tries++
+
+            if (this.tries < 2) {
+              this.tries = 0;
+              return
+            }
+          }
+        });
+      }
+    },
+
+  }
+}
+
 
 /*
 create({
@@ -126,7 +227,8 @@ window.onload = () => {
   dark.checked = true
 
   create({
-    githubAPIWrapper
+    githubAPIWrapper,
+    formHandler
   }).mount()
 
   document.addEventListener("click", changeTheme)
