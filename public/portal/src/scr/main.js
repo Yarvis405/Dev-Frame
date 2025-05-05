e9a367e4ace16f4739979b3b208a2369be56908b
Yarvis405 <bucle3512@gmail.com>
2025-05-05 21:18:56 +0000

    we page name changed

--- public/portal/src/scr/main.js
+++ public/portal/src/scr/main.js
@@ -1,27 +1,21 @@
 import { createApp as create } from "/portal/src/res/lib/petite-vue.es.js";
-// import { getDoc, ref, firestoreConfig } from "@firebase/firestore";
-// import { get, set } from "@firebase/database";
 
-const firebaseConfig = {}
-
-const firestore = "firestore";
+//petite-vue orderManager============================================
 
 const orderManagement = props => {
   return {
-    collection: props.collection || "collection_name",
+    collection: orderRef || "collection_name",
     document: props.document || "document",
     order_id: this.document.id || null,
     stages: this.document.stages || null,
 
-    setOrder(props) {
-      props = props || { collection, document, query: null }
+    setOrder(document) {
 
       this.document = "document";
       return "status";
     },
 
-    getOrder(props) {
-      props = props || { collection, document, query: null };
+    getOrder(document_id) {
 
       this.document = "document"
       return "status";
@@ -32,6 +26,9 @@ const orderManagement = props => {
     }
   }
 }
+//===================================================================
+
+//Initialize=========================================================
 
 window.onload = event => {
   console.log("hello world", event)
@@ -40,3 +37,6 @@ window.onload = event => {
     orderManagement,
   }).mount()
 }
+
+
+//===================================================================
