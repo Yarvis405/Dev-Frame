ef86b57f2586ff5626e59a236e1e52c955895f29
Yarvis405 <bucle3512@gmail.com>
2026-07-27 12:33:10 -0400

    fix(contact.tsx): fix date import and implementation, fix HTML attributes

--- app/ui/contact.tsx
+++ app/ui/contact.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-//make sure to add the scripts for proccessing and adding the date
 import { useRef, useState, useEffect } from 'react'
-import { date } from "@/app/lib/utils.client"
+import { getDate } from "@/app/lib/utils.client"
 
 const inputStyle = `apperance-none outline-none rounded-md border-solid border-2 border-gray-800 border-b-gray-400 w-full`
 const inputContainer = `flex flex-col md:text-2xl`
-const serviceStyle = `p-1 flex gap-1 items-center rounded-md has-[:checked]:bg-purple-300 has-[:checked]:text-white has-[:checked]:font-medium has-[:checked]:scale-105 transition-all active:scale-110`
+const serviceStyle = `p-1 flex gap-1 items-center rounded-md has-[:checked]:bg-purple-300 has-[:checked]:text-white has-[:checked]:font-medium has-[:checked]:scale-105 transition-all active:scale-100`
 
 const Contact = () => {
     const url = useRef("https://formsubmit.co/5d3d0e16608cd5ebb31494f00030b0f7")
+    const [currentDate, setCurrentDate] = useState<string>("")
+
+    useEffect(() => {
+        setCurrentDate(getDate())
+    }, [])
 
     return (
         <>
@@ -27,15 +31,15 @@ const Contact = () => {
 
                     <div className={inputContainer}>
                         <label htmlFor="client">Name/Org:</label>
-                        <input type="text" max="100" id="client" name="client" alt="Name or Organization" placeholder="Name or Organization" required className={inputStyle} />
+                        <input type="text" maxLength={100} id="client" name="client" alt="Name or Organization" placeholder="Name or Organization" required className={inputStyle} />
                     </div>
 
                     <div className={inputContainer}>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" max="200" id="email" name="email" alt="Type your email" placeholder="user@example.com" required className={inputStyle} />
+                        <input type="email" maxLength={200} id="email" name="email" alt="Type your email" placeholder="user@example.com" required className={inputStyle} />
                     </div>
 
-                    <input type="date" name="date" value={date} title="its suppose to be an automatic value" disabled hidden />
+                    <input type="date" name="date" value={currentDate} title="Automatic date value" disabled hidden />
 
                     <ul alt="select one of the following" className="flex nowrap gap-2 justify-between text-sm md:text-2xl">
                         <li className={serviceStyle}>
@@ -55,7 +59,7 @@ const Contact = () => {
 
                     </ul>
 
-                    <input type="submit" value="submit" alt="submit form" className="text-white text-2xl md:text-4xl bg-purple-300 font-medium rounded-md active:bg-purple-400 active:shadow-sm active:scale-105 transition" />
+                    <input type="submit" value="submit" alt="submit form" className="text-white text-2xl md:text-4xl bg-purple-300 font-medium rounded-md active:bg-purple-400 active:shadow-sm active:shadow-purple-300 transition-all" />
 
                     <p className="p-1 bg-green-500 opacity-90 text-white font-medium rounded-md text-center hidden">we will reply as soon as possible</p>
                 </form>
