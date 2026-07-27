6fc6191578a7c3a169e184b04dd79ee76eb65260
Yarvis405 <bucle3512@gmail.com>
2026-07-27 12:32:58 -0400

    fix(sliderButton.tsx): fix type annotation and remove unused imports

--- app/ui/sliderButton.tsx
+++ app/ui/sliderButton.tsx
@@ -1,33 +1,34 @@
 'use client'
 
-import { useRef, useState, useEffect } from 'react'
+import { useState } from 'react'
 
 interface buttonArgs {
     dir: number,
     value: string,
-    length: number, //content length
+    length: number | null,
     index: number,
-    setIndex: React.Dispatch<React.setStateAction<number>>,
+    setIndex: React.Dispatch<React.SetStateAction<number>>,
 }
 
 
 const slideStyles = `m-2 p-1 flex-1 text-4xl font-bold bg-gray-700 rounded-md text-white shadow-md shadow-gray-700/50 active:bg-gray-900 active:scale-70 transition duration-200 md:hidden`
 
-const SliderButton = ({dir, length, value, index, setIndex}: buttonArgs) => {
+const SliderButton = ({ dir, length, value, index, setIndex }: buttonArgs) => {
     const move = () => {
 
         if(dir === -1 && index > 0 ) {
-            setIndex(index + (dir))
-            return index
+            setIndex(index + dir)
+            return
         }
 
-        if(dir === 1 && index < length) {
-            setIndex(index + (dir))
-            return index
+        if(dir === 1 && length !== null && index < length) {
+            setIndex(index + dir)
+            return
         }
         
-        setIndex(Math.floor(length/2))
-        //setDir(dir * (-1))
+        if(length !== null) {
+            setIndex(Math.floor(length / 2))
+        }
     }
 
     return (
