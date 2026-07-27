972c50cef2c556f6db21560f1783845b59590b42
Yarvis405 <bucle3512@gmail.com>
2026-07-27 12:32:09 -0400

    fix(utils.client.ts): update with previous fixes (getDate function, type annotations)

--- app/lib/utils.client.ts
+++ app/lib/utils.client.ts
@@ -7,25 +7,24 @@
 interface parseArgs {
     data: any,
     index?: string,
-    query?: string[]
+    query: string[]
 }
 
 //utils
 
-const date = new Date().toISOString().slice(0, 10)
+const getDate = () => new Date().toISOString().slice(0, 10)
 
-const parse = async ({data, query, index} : parseArgs) => {
-    const res = [];
+const parse = async ({ data, query, index }: parseArgs) => {
+    const res: any[] = [];
 
     const obj = index 
         ? data[index]
         : data
 
-    Object.entries(obj).map(([key, value]) => {
-        const temp = {}
+    Object.entries(obj).forEach(([key, value]) => {
+        const temp: Record<string, any> = {}
 
         for(let i of query) {
-
             temp[i] = value[i]
         }
 
@@ -37,6 +36,6 @@ const parse = async ({data, query, index} : parseArgs) => {
 }
 
 export {
-    date,
+    getDate,
     parse
 }
