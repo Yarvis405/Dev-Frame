33bd784ccf050aa2349ef7217e74f9bf337d4dbd
Yarvis405 <bucle3512@gmail.com>
2026-07-27 12:24:02 -0400

    fix(utils.client.ts): fix date function, type annotations, and array method

---
+++ app/lib/utils.client.ts
@@ -0,0 +1,41 @@
+'use client'
+
+//client utils
+
+//types (in case needed)
+
+interface parseArgs {
+    data: any,
+    index?: string,
+    query: string[]
+}
+
+//utils
+
+const getDate = () => new Date().toISOString().slice(0, 10)
+
+const parse = async ({ data, query, index }: parseArgs) => {
+    const res: any[] = [];
+
+    const obj = index 
+        ? data[index]
+        : data
+
+    Object.entries(obj).forEach(([key, value]) => {
+        const temp: Record<string, any> = {}
+
+        for(let i of query) {
+            temp[i] = value[i]
+        }
+
+        res.push(temp)
+    })
+
+    return res
+
+}
+
+export {
+    getDate,
+    parse
+}
